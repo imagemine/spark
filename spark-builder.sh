3a4696f358ce549749bed579cba73b4278b0e52b
Suresh Khatri <servlet2@msn.com>
2023-08-31 18:25:40 +1000

    Update library

--- spark-builder.sh
+++ spark-builder.sh
@@ -135,7 +135,7 @@ if [[ -z $build_file ]];
 then
 ${SPARK_HOME}/bin/docker-image-tool.sh -n -r ${REPO} -t ${SPARK_VERSION} ${BUILD_PARAMS} -p ${SPARK_HOME}/kubernetes/dockerfiles/spark/Dockerfile build
 else
-  clean_unused_files $SPARK_HOME/jars
+  clean_unused_files $SPARK_HOME/jars 1
   docker build --no-cache -t ${REPO}/spark:${SPARK_VERSION} ${SPARK_HOME} -f ${build_file}
 fi;
 
